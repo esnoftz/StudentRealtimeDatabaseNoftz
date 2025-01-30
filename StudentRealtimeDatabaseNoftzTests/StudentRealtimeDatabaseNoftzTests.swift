70b00b52a61c1baf5033a037b78693434c2f71eb
esnoftz <esnoftz@gmail.com>
2025-01-30 07:37:37 -0600

    Initial Commit

---
+++ StudentRealtimeDatabaseNoftzTests/StudentRealtimeDatabaseNoftzTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudentRealtimeDatabaseNoftzTests.swift
+//  StudentRealtimeDatabaseNoftzTests
+//
+//  Created by EVANGELINE NOFTZ on 1/30/25.
+//
+
+import Testing
+@testable import StudentRealtimeDatabaseNoftz
+
+struct StudentRealtimeDatabaseNoftzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
