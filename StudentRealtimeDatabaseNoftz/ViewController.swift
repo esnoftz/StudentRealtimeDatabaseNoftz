4943924d6a6c2890a41aafd148131359e99c8a7f
esnoftz <esnoftz@gmail.com>
2025-02-05 08:04:17 -0600

    nothing works...

--- StudentRealtimeDatabaseNoftz/ViewController.swift
+++ StudentRealtimeDatabaseNoftz/ViewController.swift
@@ -23,6 +23,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     var names = [String]()
     
     var students = [Student]()
+    
+    var index = -1
+    
+    var studentName = ""
+    
+    var studentAge = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,13 +60,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBAction func saveStudentAction(_ sender: UIButton) {
         var student = Student(name: nameOutlet.text!, age: Int(ageOutlet.text!)!)
-        student.saveToFirebase()
-        students.append(student)
         
+        student.saveToFirebase()
+        
+        //ref.child("students2").childByAutoId().setValue(student)
+        students.append(student)
+                
         namesTableView.reloadData()
         
-        nameOutlet.text = ""
-        ageOutlet.text = ""
+        //nameOutlet.text = ""
+        //ageOutlet.text = ""
     }
     
     
@@ -75,8 +84,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell") as! NameCell
         //cell.nameLabel.text = "\(names[indexPath.row])"
         
+        studentName = cell.nameLabel.text!
+        studentAge = Int(cell.ageLabel.text!)!
+        
         cell.nameLabel.text = "\(students[indexPath.row].name)"
         cell.ageLabel.text = "\(students[indexPath.row].age)"
+        
+        index = indexPath.row
         
         return cell
     }
@@ -109,6 +123,36 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                     print(self.names)
                     //self.namesTableView.reloadData()
                     })
+        
+        
+        // childAdded is called every time a child is added to database
+        /*ref.child("students2").observe(.childAdded, with: { (snapshot) in
+            print(snapshot)
+
+            //let a = snapshot.value(forKey: "age") as! Int
+            //let n = snapshot.value(forKey: "name") as! String
+
+            /*if !self.students.contains(n){
+                self.names.append(n)
+            }*/
+            //self.students.append(Student(name: n, age: a))
+            //print(n)
+            self.namesTableView.reloadData()
+                    
+        })
+        
+        // when this is printed, there's nothing in names yet (this happens BEFORE the observe function)
+        print(names)
+        
+        
+        //called after .childAdded is done
+        ref.child("students2").observeSingleEvent(of: .value, with: { snapshot in
+            print("--inital load has completed and the last user was read--")
+            print(self.names)
+            //self.namesTableView.reloadData()
+        })*/
+        
+
     }
     
 
